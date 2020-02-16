3bb46bb22626ee759f0dba8bc4d20b3c8ae1cdfa
Mats <mats@plysjbyen.net>
2020-02-16 11:45:53 -0700

    formatting

--- metaconnect.js
+++ metaconnect.js
@@ -3,16 +3,16 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 
 var connected = false
 const provider = new WalletConnectProvider({
-    infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Required
-    bridge: "https://bridge.walletconnect.org/"
+  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1", // Required
+  bridge: "https://bridge.walletconnect.org/"
 });
-var a = function() {
-	function connect() {
+var a = function () {
+  function connect() {
 
     //  Enable session (triggers QR Code modal)
     if (!connected) {
       provider.enable()
-        .then(()=>{
+        .then(() => {
           console.log("yup")
           console.log(provider)
           window.web3 = new Web3(provider);
@@ -22,9 +22,9 @@ var a = function() {
         })
         .catch(console.log)
     }
-	}
+  }
 
-	function disconnect() {
+  function disconnect() {
     console.log("disconnect");
     provider.close()
       .then(() => {
@@ -32,7 +32,7 @@ var a = function() {
         console.log("disconnected")
       })
       .catch(console.log)
-	}
+  }
 
   /**
    * Listen for messages from the background script.
@@ -44,7 +44,7 @@ var a = function() {
     if (message.command === "connect") {
       connect()
     } else if (message.command === "disconnect") {
-			disconnect();
+      disconnect();
     }
   });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,9 @@
 function listenForClicks() {
   document.addEventListener("click", (e) => {
     console.log("click")
-    if (e.target.id === "connect") { 
+    if (e.target.id === "connect") {
       console.log("click: connect")
-      browser.tabs.query({active: true, currentWindow: true})
+      browser.tabs.query({ active: true, currentWindow: true })
         .then((tabs) => {
           console.log("click: connect: send")
           connected = true
@@ -12,8 +12,8 @@ function listenForClicks() {
           });
         })
         .catch(console.log)
-		} else if (e.target.id  === "disconnect") {
-      browser.tabs.query({active: true, currentWindow: true})
+    } else if (e.target.id === "disconnect") {
+      browser.tabs.query({ active: true, currentWindow: true })
         .then((tabs) => {
           browser.tabs.sendMessage(tabs[0].id, {
             command: "disconnect",
@@ -25,6 +25,6 @@ function listenForClicks() {
   });
 }
 
-browser.tabs.executeScript({file: "/metaconnect.js"})
+browser.tabs.executeScript({ file: "/metaconnect.js" })
   .then(listenForClicks)
   .catch(console.log);
